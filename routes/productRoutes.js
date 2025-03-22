43322881e639b800f83301671271e141d1735f5e
Libraaa <annvps41218@gmail.com>
2025-03-22 16:59:31 +0700

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -15,17 +15,18 @@ router.get("/all", async (req, res) => {
 // Lọc sản phẩm theo category (dựa trên tags)
 router.get("/category", async (req, res) => {
     try {
-        const { tag } = req.query; // Nhận category từ query
-        if (!tag) {
-            return res.status(400).json({ message: "Vui lòng cung cấp category" });
+        const { category } = req.query; // Nhận danh mục từ query
+        if (!category) {
+            return res.status(400).json({ message: "Vui lòng cung cấp danh mục" });
         }
 
-        const products = await Product.find({ tags: { $in: [tag] } });
+        const products = await Product.find({ category: category });
         res.json(products);
     } catch (error) {
         res.status(500).json({ message: "Lỗi server", error });
     }
 });
+
 
 // Tìm kiếm sản phẩm theo tên
 router.get("/search", async (req, res) => {
