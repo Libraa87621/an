040b029b100407ad2a10534cb7371bb7388c5459
Libraaa <annvps41218@gmail.com>
2025-03-22 16:30:26 +0700

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -12,6 +12,20 @@ router.get("/all", async (req, res) => {
         res.status(500).json({ message: "Lỗi server", error });
     }
 });
+// Lọc sản phẩm theo category (dựa trên tags)
+router.get("/category", async (req, res) => {
+    try {
+        const { tag } = req.query; // Nhận category từ query
+        if (!tag) {
+            return res.status(400).json({ message: "Vui lòng cung cấp category" });
+        }
+
+        const products = await Product.find({ tags: { $in: [tag] } });
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ message: "Lỗi server", error });
+    }
+});
 
 // Tìm kiếm sản phẩm theo tên
 router.get("/search", async (req, res) => {
