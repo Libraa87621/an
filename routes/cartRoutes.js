a5a61ad48de550d461e7083b98eb19b232452986
Libraaa <annvps41218@gmail.com>
2025-04-17 08:30:26 +0700

    a
    a

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -42,7 +42,7 @@ router.post("/add", async (req, res) => {
           productId,
           name: product.name,
           price: product.price,
-          img: product.image, 
+          image: product.image, 
           quantity,
           total,
         });
